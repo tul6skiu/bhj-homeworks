50a6dfa8dcbe7905684fe57626032ec734406295
wlistertula <tulaa478@gmail.com>
2025-05-10 23:18:22 +0300

    keysolo

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -17,14 +17,16 @@ class Game {
   }
 
   registerEvents() {
-    /*
-      TODO:
-      Написать обработчик события, который откликается
-      на каждый введённый символ.
-      В случае правильного ввода символа вызываем this.success()
-      При неправильном вводе символа - this.fail();
-      DOM-элемент текущего символа находится в свойстве this.currentSymbol.
-     */
+    document.addEventListener('keydown', (event) => {
+      const pressedKey = event.key;
+      const currentChar = this.currentSymbol.textContent;
+
+      if (pressedKey.toLocaleLowerCase() === currentChar.toLocaleLowerCase()) {
+        this.success();
+      }else {
+        this.fail();
+      }
+    });
   }
 
   success() {
